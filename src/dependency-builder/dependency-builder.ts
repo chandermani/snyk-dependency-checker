8633302299c0883f22865ffdf4823cf9c02c53d7
chandermani <chandermani@outlook.com>
2021-06-01 19:09:08 +0100

    Adding some basic caching

--- src/dependency-builder/dependency-builder.ts
+++ src/dependency-builder/dependency-builder.ts
@@ -3,21 +3,33 @@ import { maxSatisfying } from 'semver';
 import { Package } from './package';
 import { packageNpmUrl, packageNpmVersionedUrl } from './uri-builder';
 
+type PackageMetadata = {
+  name: string;
+  version: string;
+  dependencies: { [key: string]: string };
+};
+
 export async function buildDependencyGraph(packageName: string, version: string): Promise<Package> {
   const packageIdentifier = (packageName, version) => `${packageName}-${version}`;
   const dummyRoot: Package = { name: 'dummy', version: '1', dependencies: [] };
 
+  const packageVersionMetaCache: { [key: string]: PackageMetadata } = {};
   const packageProcessingQueue = [{ packageName, version, parent: dummyRoot }];
 
   const visited: { [key: string]: boolean } = {};
   visited[packageIdentifier(packageName, version)] = true;
 
+  const packageMetadataResponse = await getPackageVersionMetaData({ packageName, version });
+  packageVersionMetaCache[packageIdentifier(packageName, version)] = packageMetadataResponse.data;
+
   while (packageProcessingQueue.length > 0) {
     const packageToProcess = packageProcessingQueue.shift();
 
-    const packageMetadataRequest = await getDependencyMetaData(packageToProcess);
-
-    const { name, dependencies, version: packageVersion } = packageMetadataRequest.data;
+    const {
+      name,
+      version: packageVersion,
+      dependencies,
+    } = packageVersionMetaCache[packageIdentifier(packageToProcess.packageName, packageToProcess.version)];
 
     const newPackage: Package = {
       name,
@@ -28,7 +40,9 @@ export async function buildDependencyGraph(packageName: string, version: string)
     packageToProcess.parent.dependencies.push(newPackage);
 
     for (const dependencyName in dependencies ?? {}) {
-      const dependencyVersion = await getNormalizeVersion(dependencyName, dependencies[dependencyName]);
+      const depedencyMetadata = await getPackageMetadata(dependencyName);
+      const dependencyVersion = maxSatisfying(Object.keys(depedencyMetadata.versions), dependencies[dependencyName]).toString();
+      packageVersionMetaCache[packageIdentifier(dependencyName, dependencyVersion)] = depedencyMetadata.versions[dependencyVersion];
       if (!visited[packageIdentifier(dependencyName, dependencyVersion)]) {
         visited[packageIdentifier(dependencyName, dependencyVersion)] = true;
         packageProcessingQueue.push({ packageName: dependencyName, version: dependencyVersion, parent: newPackage });
@@ -38,7 +52,7 @@ export async function buildDependencyGraph(packageName: string, version: string)
 
   return dummyRoot.dependencies[0];
 }
-async function getDependencyMetaData(packageToProcess: { packageName: string; version: string; parent: Package }) {
+async function getPackageVersionMetaData(packageToProcess: { packageName: string; version: string }) {
   try {
     return await axios.get<{ name: string; version: string; dependencies: { [key: string]: string } }>(
       packageNpmVersionedUrl(packageToProcess.packageName, packageToProcess.version),
@@ -48,10 +62,11 @@ async function getDependencyMetaData(packageToProcess: { packageName: string; ve
   }
 }
 
-async function getNormalizeVersion(dependencyName, dependencyVersion): Promise<string> {
+async function getPackageMetadata(dependencyName) {
   try {
-    const response = await axios.get<{ versions: { [key: string]: unknown } }>(packageNpmUrl(dependencyName));
-    return maxSatisfying(Object.keys(response.data.versions), dependencyVersion).toString();
+    return await (
+      await axios.get<{ name: string; versions: { [key: string]: PackageMetadata } }>(packageNpmUrl(dependencyName))
+    ).data;
   } catch {
     // todo: Handling exception
   }
